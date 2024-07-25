63799db2b43f73e2bf0bf991c4e948cb76e68afd
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-07-25 09:27:04 +0300

    use the post router in our app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application } from "express";
 import { userRouter } from "./routes/userRoutes";
+import { postRouter } from "./routes/postsRoutes";
 
 const app: Application = express();
 //user routes
 app.use(userRouter);
+app.use(postRouter);
 
 app.listen(3000, () => {
   console.log("the app is running ion port 3000");
