dc09bc3445897de4a96a1d5be88b5a812b4fa48e
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-07-31 23:05:32 +0300

    use the categories routes in the express app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import { userRouter } from "./routes/userRoutes";
 import { postRouter } from "./routes/postsRoutes";
+import { categoriesRouter } from "./routes/categoriesRoutes";
 
 const app: Application = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
+app.use("/api/categories", categoriesRouter);
 
 export default app;
